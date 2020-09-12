1d2850c2f875c0fb241ab36352c221a9a4d7ffa3
Blanca Gomez <blancagoand@gmail.com>
2020-09-12 14:53:05 -0700

    firebase auth working with facebook and google

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,35 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const firebase = require('firebase/app');
+const firebaseui = require('firebaseui');
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import "firebase/auth";
+import "firebase/firestore";
 import $ from 'jquery';
-import List from './components/List.jsx';
+import firebaseConfig from './firebaseConfig';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
-    }
+      isSignedIn: undefined,
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
+    // Configure FirebaseUI.
+    this.uiConfig = {
+      // Popup signin flow rather than redirect flow.
+      signInFlow: 'popup',
+      // We will display Google and Facebook as auth providers.
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID
+      ],
+      callbacks: {
+        signInSuccessWithAuthResult: () => false,
+      },
+    };
   }
 
   componentDidMount() {
-    $.ajax({
-      url: '/items',
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged((user) => {
+      this.setState({isSignedIn: !!user});
     });
+  }
+
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
   }
 
   render () {
     return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+        {this.state.isSignedIn !== undefined && !this.state.isSignedIn &&
+          <div>
+            <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+          </div>
+        }
+        {this.state.isSignedIn &&
+          <div>
+            Hello {firebase.auth().currentUser.displayName}. You are now signed In!
+            <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+          </div>
+        }
+      </div>);
   }
 }
 
